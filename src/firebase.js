ad85b84e09208bcb40ab16a7bde2ca61916686aa
gauravsah21 <gsah.analytics@gmail.com>
2024-02-15 14:19:26 +0545

    first comit

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBXaqMRWSB1u4C1KXzyDnct3zJWwnoE6Sk",
+  authDomain: "new-project-480da.firebaseapp.com",
+  projectId: "new-project-480da",
+  storageBucket: "new-project-480da.appspot.com",
+  messagingSenderId: "91741277682",
+  appId: "1:91741277682:web:b3bf21850c75d96285ef6b",
+  measurementId: "G-4759V4ZRT4"
+};
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
